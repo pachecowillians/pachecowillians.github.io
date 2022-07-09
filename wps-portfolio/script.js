8eb2d2bf3196ccd39b96b047b37eb8b9444b364a
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-09 01:25:06 -0300

    Improve Dark to Light mode Toggle

--- wps-portfolio/script.js
+++ wps-portfolio/script.js
@@ -1,25 +1,26 @@
 var darkTheme = false;
 
 document.querySelector("#theme-toggle").onclick = function() {
+    let properties = [
+        "background",
+        "darkgrey",
+        "darkblue",
+        "black",
+        "lightblue",
+        "grey",
+        "white",
+        "menu-option"
+    ];
+
     if (!darkTheme) {
-        document.documentElement.style.setProperty("--background", "var(--background-dark)");
-        document.documentElement.style.setProperty("--darkgrey", "var(--darkgrey-dark)");
-        document.documentElement.style.setProperty("--darkblue", "var(--darkblue-dark)");
-        document.documentElement.style.setProperty("--black", "var(--black-dark)");
-        document.documentElement.style.setProperty("--lightblue", "var(--lightblue-dark)");
-        document.documentElement.style.setProperty("--grey", "var(--grey-dark)");
-        document.documentElement.style.setProperty("--white", "var(--white-dark)");
-        document.documentElement.style.setProperty("--menu-option", "var(--white-dark)");
+        properties.map(prop => {
+            document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-dark)`);
+        });
         document.querySelector("#logo").style.setProperty("filter", "invert(99%) sepia(0%) saturate(0%) hue-rotate(148deg) brightness(87%) contrast(91%)");
     } else {
-        document.documentElement.style.setProperty("--background", "var(--background-light)");
-        document.documentElement.style.setProperty("--darkgrey", "var(--darkgrey-light)");
-        document.documentElement.style.setProperty("--darkblue", "var(--darkblue-light)");
-        document.documentElement.style.setProperty("--black", "var(--black-light)");
-        document.documentElement.style.setProperty("--lightblue", "var(--lightblue-light)");
-        document.documentElement.style.setProperty("--grey", "var(--grey-light)");
-        document.documentElement.style.setProperty("--white", "var(--white-light)");
-        document.documentElement.style.setProperty("--menu-option", "var(--black-light)");
+        properties.map(prop => {
+            document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-light)`);
+        });
         document.querySelector("#logo").style.setProperty("filter", "none");
     }
     darkTheme = !darkTheme;
