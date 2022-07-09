d4d210ac171a2dec7b0650e2b622e663a951431b
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-09 02:06:43 -0300

    Responsivity of navbar

--- wps-portfolio/script.js
+++ wps-portfolio/script.js
@@ -16,12 +16,12 @@ document.querySelector("#theme-toggle").onclick = function() {
         properties.map(prop => {
             document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-dark)`);
         });
-        document.querySelector("#logo").style.setProperty("filter", "invert(99%) sepia(0%) saturate(0%) hue-rotate(148deg) brightness(87%) contrast(91%)");
+        document.querySelector("#logoNavbar").style.setProperty("filter", "invert(99%) sepia(0%) saturate(0%) hue-rotate(148deg) brightness(87%) contrast(91%)");
     } else {
         properties.map(prop => {
             document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-light)`);
         });
-        document.querySelector("#logo").style.setProperty("filter", "none");
+        document.querySelector("#logoNavbar").style.setProperty("filter", "none");
     }
     darkTheme = !darkTheme;
 }
