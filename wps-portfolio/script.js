4604c9fd92f7be218e2c3426f3b56b59b7e8490d
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-09 10:58:11 -0300

    Update logo color on darkmode

--- wps-portfolio/script.js
+++ wps-portfolio/script.js
@@ -17,11 +17,13 @@ document.querySelector("#theme-toggle").onclick = function() {
             document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-dark)`);
         });
         document.querySelector("#logoNavbar").style.setProperty("filter", "invert(99%) sepia(0%) saturate(0%) hue-rotate(148deg) brightness(87%) contrast(91%)");
+        document.querySelector("#logoTopbar").style.setProperty("filter", "invert(99%) sepia(0%) saturate(0%) hue-rotate(148deg) brightness(87%) contrast(91%)");
     } else {
         properties.map(prop => {
             document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-light)`);
         });
         document.querySelector("#logoNavbar").style.setProperty("filter", "none");
+        document.querySelector("#logoTopbar").style.setProperty("filter", "none");
     }
     darkTheme = !darkTheme;
 }
