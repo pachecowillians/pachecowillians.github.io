58649fb3148027eb3e21bbdd32861a14f0035dc3
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-12 09:21:10 -0300

    Fix comma problem in map

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var darkTheme = false;
 var selected = "profile";
+let properties = [
+    "background",
+    "darkgrey",
+    "darkblue",
+    "black",
+    "lightblue",
+    "grey",
+    "white",
+    "text"
+];
 let sections = [
     "profile",
     "programming",
@@ -7,19 +17,30 @@ let sections = [
     "professional",
     "projects"
 ];
-
+let languages = [
+    { name: 'HTML', percentage: 95, color: '#E44D26' },
+    { name: 'CSS', percentage: 85, color: '#1572B6' },
+    { name: 'JavaScript', percentage: 80, color: '#1572B6' },
+    { name: 'TypeScript', percentage: 60, color: '#1572B6' },
+    { name: 'React JS', percentage: 50, color: '#1572B6' },
+    { name: 'Next JS', percentage: 50, color: '#1572B6' },
+    { name: 'Bootstrap', percentage: 75, color: '#1572B6' },
+    { name: 'Flask', percentage: 75, color: '#1572B6' },
+    { name: 'PHP', percentage: 75, color: '#1572B6' },
+    { name: 'Python', percentage: 85, color: '#1572B6' },
+    { name: 'Java', percentage: 40, color: '#1572B6' },
+    { name: 'C', percentage: 90, color: '#1572B6' },
+    { name: 'C++', percentage: 85, color: '#1572B6' },
+    { name: 'C#', percentage: 40, color: '#1572B6' },
+    { name: 'Arch Linux', percentage: 80, color: '#1572B6' },
+    { name: 'Ubuntu', percentage: 85, color: '#1572B6' },
+    { name: 'Windows', percentage: 60, color: '#1572B6' },
+    { name: 'SQL Server', percentage: 60, color: '#1572B6' },
+    { name: 'MySQl', percentage: 85, color: '#1572B6' },
+    { name: 'Docker', percentage: 85, color: '#1572B6' },
+    { name: 'Git', percentage: 90, color: '#1572B6' },
+];
 document.querySelector("#theme-toggle").onclick = function() {
-    let properties = [
-        "background",
-        "darkgrey",
-        "darkblue",
-        "black",
-        "lightblue",
-        "grey",
-        "white",
-        "text"
-    ];
-
     if (!darkTheme) {
         properties.map(prop => {
             document.documentElement.style.setProperty(`--${prop}`, `var(--${prop}-dark)`);
@@ -53,22 +74,10 @@ sections.map(section => {
     }
 });
 
-let languages = [{
-        name: 'HTML',
-        percentage: 95,
-        color: '#E44D26',
-    },
-    {
-        name: 'CSS',
-        percentage: 85,
-        color: '#1572B6',
-    }
-]
-
 document.querySelector("#programming").innerHTML = languages.map(
     (language) => (`
-            <div class="language-container ${language.name.toLowerCase()}">
-                <img src="img/Languages/${language.name.toLowerCase()}.svg" alt="${language.name}">
+            <div class="language-container ${language.name.toLowerCase().replace(' ','')}">
+                <img src="img/Languages/${language.name.toLowerCase().replace(' ','')}.svg" alt="${language.name}">
                 <div class="progress-bar-container">
                     <div class="progress-bar-title">
                         <span>${language.name}</span>
@@ -79,23 +88,23 @@ document.querySelector("#programming").innerHTML = languages.map(
                 </div>
             </div>
             `)
-);
+).join('');
 
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
 
 languages.map((language) => {
     console.log(language);
     addCSS(`
-    .${language.name.toLowerCase()} .progress-bar::before {
+    .${language.name.toLowerCase().replace(' ','')} .progress-bar::before {
         background: ${language.color};
         width: ${language.percentage}%;
     }
    
-    .${language.name.toLowerCase()} .progress-bar {
+    .${language.name.toLowerCase().replace(' ','')} .progress-bar {
         border: 1px solid ${language.color};
     }
     
-    .${language.name.toLowerCase()} {
+    .${language.name.toLowerCase().replace(' ','')} {
         color: ${language.color};
     }
     `);
