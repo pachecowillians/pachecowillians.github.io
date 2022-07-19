c29a242088fd09e15848da9b2b60a87ab179e186
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-19 18:01:51 -0300

    Professional information

--- data/professional.js
+++ data/professional.js
@@ -1,21 +1,24 @@
-export const informationText = "I am at the beginning of my professional career and i am looking for opportunities to apply my knowledge working in a company. Below are my experiences in the job market."
+export const informationText = "Estou no começo da minha carreira professional, a procura de oportunidades para aplicar os meus conhecimentos no mercado de trabalho. Abaixo estão listadas as minhas experiências profissionais."
 
 export const professionalItems = [{
     name: 'Controle Eletrônica',
     referenceName: 'controleeletronica',
-    date: '2018 - 2020',
+    date: '2015 - 2018',
     link: 'https://controleeletronica.com.br',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description: "Empresa familiar que realiza a fabricação e manutenção de produtos eletrônicos, tais como reles temporizadores, sequenciadoras, indicadores e temporizadores digitais e reles de interface.",
     tasks: [
-        'PCB',
-        'Program',
-        'Inventory control',
-        'ERP'
+        'Implementação e manutenção do site da empresa',
+        'Manutenção do ERP da empresa',
+        'Programação de microcontroladores',
+        'Análise de estoque',
+        'Desenvolvimento e otimização dos produtos',
+        'Montagem e manutenção de eletrônicos',
+        'Operação de máquinas pick and place'
     ]
 }]
 
 export const titles = {
-    website: 'Website',
-    description: 'Description',
-    tasks: 'Tasks'
+    website: 'Site',
+    description: 'Descrição',
+    tasks: 'Tarefas'
 }
