2216bbd9fd94d077fac8f70e8dcf604571fed492
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-19 21:15:10 -0300

    Programming data

--- data/programming.js
+++ data/programming.js
@@ -1,129 +1,129 @@
-export const informationText = 'During my life, I learned to use several programming languages, technologies and operating systems. The main knowledge obtained is listed here based on how much I know about each one.';
+export const informationText = 'Durante meus estudos, aprendi a usar várias linguagens de programação, tecnologias e sistemas operacionais. Os conhecimentos adquiridos são listados aqui com base no quanto eu sei de cada assunto.'
 
 export let languages = [{
         name: 'HTML',
         referenceName: 'html',
-        percentage: 95,
+        percentage: ((60 / 117) * 100).toFixed(2),
         color: '#E44D26'
     },
     {
         name: 'CSS',
         referenceName: 'css',
-        percentage: 85,
+        percentage: ((137 / 228) * 100).toFixed(2),
         color: '#1572B6'
     },
     {
         name: 'JavaScript',
         referenceName: 'javascript',
-        percentage: 80,
+        percentage: ((46 / 65) * 100).toFixed(2),
         color: '#F0DB4F'
     },
     {
         name: 'TypeScript',
         referenceName: 'typescript',
-        percentage: 60,
+        percentage: ((8 / 16) * 100).toFixed(2),
         color: '#007ACC'
     },
     {
         name: 'React JS',
         referenceName: 'reactjs',
-        percentage: 50,
+        percentage: ((9 / 12) * 100).toFixed(2),
         color: '#61DAFB'
     },
     {
         name: 'Next JS',
         referenceName: 'nextjs',
-        percentage: 50,
+        percentage: ((6 / 17) * 100).toFixed(2),
         color: 'var(--text)'
     },
     {
         name: 'Bootstrap',
         referenceName: 'bootstrap',
-        percentage: 75,
+        percentage: ((15 / 21) * 100).toFixed(2),
         color: '#7952B3'
     },
     {
         name: 'Flask',
         referenceName: 'flask',
-        percentage: 75,
+        percentage: ((6 / 16) * 100).toFixed(2),
         color: 'var(--text)'
     },
     {
         name: 'PHP',
         referenceName: 'php',
-        percentage: 75,
+        percentage: ((8 / 22) * 100).toFixed(2),
         color: '#6181B6'
     },
     {
         name: 'Python',
         referenceName: 'python',
-        percentage: 85,
+        percentage: ((30 / 317) * 100).toFixed(2),
         color: '#386F9F'
     },
     {
         name: 'Java',
         referenceName: 'java',
-        percentage: 40,
+        percentage: ((12 / 31) * 100).toFixed(2),
         color: '#EA2D2E'
     },
     {
         name: 'C',
         referenceName: 'c',
-        percentage: 90,
+        percentage: ((10 / 15) * 100).toFixed(2),
         color: '#03599C'
     },
     {
         name: 'C++',
         referenceName: 'cplusplus',
-        percentage: 85,
+        percentage: ((11 / 25) * 100).toFixed(2),
         color: '#9C033A'
     },
     {
         name: 'C#',
         referenceName: 'csharp',
-        percentage: 40,
+        percentage: ((7 / 14) * 100).toFixed(2),
         color: '#68217A'
     },
     {
         name: 'Arch Linux',
         referenceName: 'archlinux',
-        percentage: 80,
+        percentage: ((15 / 22) * 100).toFixed(2),
         color: '#1793D1'
     },
     {
         name: 'Ubuntu',
         referenceName: 'ubuntu',
-        percentage: 85,
+        percentage: ((6 / 8) * 100).toFixed(2),
         color: '#DD4814'
     },
     {
         name: 'Windows',
         referenceName: 'windows',
-        percentage: 60,
+        percentage: ((4 / 8) * 100).toFixed(2),
         color: '#0078D4'
     },
     {
         name: 'SQL Server',
         referenceName: 'sqlserver',
-        percentage: 60,
+        percentage: ((28 / 38) * 100).toFixed(2),
         color: '#A91D22'
     },
     {
         name: 'MySQl',
         referenceName: 'mysql',
-        percentage: 85,
+        percentage: ((28 / 38) * 100).toFixed(2),
         color: '#00618A'
     },
     {
         name: 'Docker',
         referenceName: 'docker',
-        percentage: 85,
+        percentage: ((5 / 8) * 100).toFixed(2),
         color: '#028BB8'
     },
     {
         name: 'Git',
         referenceName: 'git',
-        percentage: 90,
+        percentage: ((19 / 44) * 100).toFixed(2),
         color: '#F34F29'
     },
 ];
