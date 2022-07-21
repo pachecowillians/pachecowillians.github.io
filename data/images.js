33a1ea4fe325fdd0b3cb5960ab6b7c11e94cab93
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-20 22:43:10 -0300

    Remove filter attribute of images data file

--- data/images.js
+++ data/images.js
@@ -1,211 +1,171 @@
 const companies = [{
     name: 'controleeletronica',
     path: 'img/Companies/controleeletronica.png',
-    filter: true
 }]
 
 const languages = [{
         name: 'archlinux',
         path: 'img/Languages/archlinux.svg',
-        filter: false
     },
     {
         name: 'bootstrap',
         path: 'img/Languages/bootstrap.svg',
-        filter: false
     },
     {
         name: 'c',
         path: 'img/Languages/c.svg',
-        filter: false
     },
     {
         name: 'cplusplus',
         path: 'img/Languages/cplusplus.svg',
-        filter: false
     },
     {
         name: 'csharp',
         path: 'img/Languages/csharp.svg',
-        filter: false
     },
     {
         name: 'css',
         path: 'img/Languages/css.svg',
-        filter: false
     },
     {
         name: 'docker',
         path: 'img/Languages/docker.svg',
-        filter: false
     },
     {
         name: 'dotnet',
         path: 'img/Languages/dotnet.svg',
-        filter: false
     },
     {
         name: 'flask',
         path: 'img/Languages/flask.svg',
-        filter: true
     },
     {
         name: 'git',
         path: 'img/Languages/git.svg',
-        filter: false
     },
     {
         name: 'html',
         path: 'img/Languages/html.svg',
-        filter: false
     },
     {
         name: 'java',
         path: 'img/Languages/java.svg',
-        filter: false
     },
     {
         name: 'javascript',
         path: 'img/Languages/javascript.svg',
-        filter: false
     },
     {
         name: 'jquery',
         path: 'img/Languages/jquery.svg',
-        filter: false
     },
     {
         name: 'mysql',
         path: 'img/Languages/mysql.svg',
-        filter: false
     },
     {
         name: 'nextjs',
         path: 'img/Languages/nextjs.svg',
-        filter: true
     },
     {
         name: 'nodejs',
         path: 'img/Languages/nodejs.svg',
-        filter: true
     },
     {
         name: 'php',
         path: 'img/Languages/php.svg',
-        filter: false
     },
     {
         name: 'postgresql',
         path: 'img/Languages/postgresql.svg',
-        filter: false
     },
     {
         name: 'python',
         path: 'img/Languages/python.svg',
-        filter: false
     },
     {
         name: 'reactjs',
         path: 'img/Languages/reactjs.svg',
-        filter: false
     },
     {
         name: 'sass',
         path: 'img/Languages/sass.svg',
-        filter: false
     },
     {
         name: 'sqlserver',
         path: 'img/Languages/sqlserver.svg',
-        filter: false
     },
     {
         name: 'typescript',
         path: 'img/Languages/typescript.svg',
-        filter: false
     },
     {
         name: 'ubuntu',
         path: 'img/Languages/ubuntu.svg',
-        filter: false
     },
     {
         name: 'windows',
         path: 'img/Languages/windows.svg',
-        filter: false
     }
 ]
 
 const logo = [{
         name: 'logo',
         path: 'img/Logo/logo.svg',
-        filter: true
     },
     {
         name: 'favicon',
         path: 'img/Logo/favicon.svg',
-        filter: false
     }
 ]
 
 const projects = [{
         name: 'wpstimer',
         path: 'img/Projects/wpstimer.gif',
-        filter: false
     },
     {
         name: 'catalogueit',
         path: 'img/Projects/catalogueit.gif',
-        filter: false
     },
     {
         name: 'whatsappclone',
         path: 'img/Projects/whatsappclone.gif',
-        filter: false
     },
     {
         name: 'tcctecnico',
         path: 'img/Projects/tcctecnico.gif',
-        filter: false
     },
     {
         name: 'cs50',
         path: 'img/Projects/cs50.gif',
-        filter: false
     },
     {
         name: 'quimicaorganica',
         path: 'img/Projects/quimicaorganica.gif',
-        filter: false
     }
 ]
 
 const social = [{
         name: 'github',
         path: 'img/Social/github.svg',
-        filter: true
     },
     {
         name: 'githubwhite',
         path: 'img/Social/githubwhite.svg',
-        filter: false
     },
     {
         name: 'gmail',
         path: 'img/Social/gmail.svg',
-        filter: false
     },
     {
         name: 'linkedin',
         path: 'img/Social/linkedin.svg',
-        filter: false
     }
 ]
 
 const profile = [{
     name: 'profile',
     path: 'img/profile.png',
-    filter: false
 }]
 
 export const images = companies.concat(languages).concat(logo).concat(projects).concat(social).concat(profile);
