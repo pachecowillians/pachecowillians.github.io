4122ebcd080c9136ef6ff4d2fab825620451f842
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-07-25 07:12:14 -0300

    Fix sections scroll

--- utils/navbar.js
+++ utils/navbar.js
@@ -1,19 +1,22 @@
-import { isSectionInViewport } from "./viewport.js";
-// import { sections } from "../data/sections.js";
 import { $ } from "./selector.js";
+import { isSectionInViewport } from "./viewport.js";
+import { navbarData } from "../data/sections.js";
+import { getCurrentLanguage } from "../utils/translate.js"
+
+let sections = navbarData[getCurrentLanguage()];
 
 export function setActiveSection(oldSection) {
-    // var i = 0;
+    var i = 0;
 
-    // while (i < sections.length && !isSectionInViewport($(`#${sections[i].referenceName}`))) {
-    //     i++;
-    // }
+    while (i < sections.length && !isSectionInViewport($(`#${sections[i].referenceName}`))) {
+        i++;
+    }
 
-    // if (oldSection != sections[i].referenceName) {
-    //     let oldIcon = $(".selected-item");
-    //     oldIcon.classList.remove("selected-item");
+    if (oldSection != sections[i].referenceName) {
+        let oldIcon = $(".selected-item");
+        oldIcon.classList.remove("selected-item");
 
-    //     let icon = $(`#${sections[i].referenceName}Icon`);
-    //     icon.classList.add("selected-item");
-    // }
+        let icon = $(`#${sections[i].referenceName}Icon`);
+        icon.classList.add("selected-item");
+    }
 }
