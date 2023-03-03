a108c7044b2cf97e853221f5845770ea3bf2d6b7
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-03-03 14:39:59 -0300

    Removing percentage from programming languages

--- data/programming.js
+++ data/programming.js
@@ -2,321 +2,269 @@ export const programmingData = {
     portuguese: {
         informationText: 'Durante meus estudos, aprendi várias linguagens de programação, tecnologias novas e sistemas operacionais. Os conhecimentos adquiridos, tecnologias e linguagens dominadas são aqui apresentados.',
         languages: [{
-                name: 'HTML',
-                referenceName: 'html',
-                percentage: ((60 / 117) * 100).toFixed(2),
-                color: '#E44D26'
-            },
-            {
-                name: 'CSS',
-                referenceName: 'css',
-                percentage: ((137 / 228) * 100).toFixed(2),
-                color: '#1572B6'
-            },
-            {
-                name: 'JavaScript',
-                referenceName: 'javascript',
-                percentage: ((46 / 65) * 100).toFixed(2),
-                color: '#F0DB4F'
-            },
-            {
-                name: 'TypeScript',
-                referenceName: 'typescript',
-                percentage: ((8 / 16) * 100).toFixed(2),
-                color: '#007ACC'
-            },
-            {
-                name: 'React JS',
-                referenceName: 'reactjs',
-                percentage: ((9 / 12) * 100).toFixed(2),
-                color: '#61DAFB'
-            },
-            {
-                name: 'Next JS',
-                referenceName: 'nextjs',
-                percentage: ((6 / 17) * 100).toFixed(2),
-                color: 'var(--text)'
-            },
-            {
-                name: 'Node JS',
-                referenceName: 'nodejs',
-                percentage: ((3 / 59) * 100).toFixed(2),
-                color: '#83CD29'
-            },
-            {
-                name: 'Sass',
-                referenceName: 'sass',
-                percentage: ((4 / 60) * 100).toFixed(2),
-                color: '#CB6699'
-            },
-            {
-                name: 'Bootstrap',
-                referenceName: 'bootstrap',
-                percentage: ((15 / 21) * 100).toFixed(2),
-                color: '#7952B3'
-            },
-            {
-                name: 'jQuery',
-                referenceName: 'jquery',
-                percentage: ((80 / 318) * 100).toFixed(2),
-                color: '#0868AC'
-            },
-            {
-                name: 'Flask',
-                referenceName: 'flask',
-                percentage: ((6 / 16) * 100).toFixed(2),
-                color: 'var(--text)'
-            },
-            {
-                name: 'PHP',
-                referenceName: 'php',
-                percentage: ((8 / 22) * 100).toFixed(2),
-                color: '#6181B6'
-            },
-            {
-                name: 'Python',
-                referenceName: 'python',
-                percentage: ((30 / 317) * 100).toFixed(2),
-                color: '#386F9F'
-            },
-            {
-                name: 'Java',
-                referenceName: 'java',
-                percentage: ((12 / 31) * 100).toFixed(2),
-                color: '#EA2D2E'
-            },
-            {
-                name: 'C',
-                referenceName: 'c',
-                percentage: ((10 / 15) * 100).toFixed(2),
-                color: '#03599C'
-            },
-            {
-                name: 'C++',
-                referenceName: 'cplusplus',
-                percentage: ((11 / 25) * 100).toFixed(2),
-                color: '#9C033A'
-            },
-            {
-                name: 'C#',
-                referenceName: 'csharp',
-                percentage: ((7 / 14) * 100).toFixed(2),
-                color: '#68217A'
-            },
-            {
-                name: '.NET',
-                referenceName: 'dotnet',
-                percentage: ((13 / 182) * 100).toFixed(2),
-                color: '#198AD0'
-            },
-            {
-                name: 'Arch Linux',
-                referenceName: 'archlinux',
-                percentage: ((15 / 22) * 100).toFixed(2),
-                color: '#1793D1'
-            },
-            {
-                name: 'Ubuntu',
-                referenceName: 'ubuntu',
-                percentage: ((6 / 8) * 100).toFixed(2),
-                color: '#DD4814'
-            },
-            {
-                name: 'Windows',
-                referenceName: 'windows',
-                percentage: ((4 / 8) * 100).toFixed(2),
-                color: '#0078D4'
-            },
-            {
-                name: 'SQL Server',
-                referenceName: 'sqlserver',
-                percentage: ((28 / 38) * 100).toFixed(2),
-                color: '#A91D22'
-            },
-            {
-                name: 'MySQl',
-                referenceName: 'mysql',
-                percentage: ((28 / 38) * 100).toFixed(2),
-                color: '#00618A'
-            },
-            {
-                name: 'PostgreSQL',
-                referenceName: 'postgresql',
-                percentage: ((28 / 38) * 100).toFixed(2),
-                color: '#336791'
-            },
-            {
-                name: 'Docker',
-                referenceName: 'docker',
-                percentage: ((5 / 8) * 100).toFixed(2),
-                color: '#028BB8'
-            },
-            {
-                name: 'Git',
-                referenceName: 'git',
-                percentage: ((19 / 44) * 100).toFixed(2),
-                color: '#F34F29'
-            },
+            name: 'HTML',
+            referenceName: 'html',
+            color: '#E44D26'
+        },
+        {
+            name: 'CSS',
+            referenceName: 'css',
+            color: '#1572B6'
+        },
+        {
+            name: 'JavaScript',
+            referenceName: 'javascript',
+            color: '#F0DB4F'
+        },
+        {
+            name: 'TypeScript',
+            referenceName: 'typescript',
+            color: '#007ACC'
+        },
+        {
+            name: 'React JS',
+            referenceName: 'reactjs',
+            color: '#61DAFB'
+        },
+        {
+            name: 'Next JS',
+            referenceName: 'nextjs',
+            color: 'var(--text)'
+        },
+        {
+            name: 'Node JS',
+            referenceName: 'nodejs',
+            color: '#83CD29'
+        },
+        {
+            name: 'Sass',
+            referenceName: 'sass',
+            color: '#CB6699'
+        },
+        {
+            name: 'Bootstrap',
+            referenceName: 'bootstrap',
+            color: '#7952B3'
+        },
+        {
+            name: 'jQuery',
+            referenceName: 'jquery',
+            color: '#0868AC'
+        },
+        {
+            name: 'Flask',
+            referenceName: 'flask',
+            color: 'var(--text)'
+        },
+        {
+            name: 'PHP',
+            referenceName: 'php',
+            color: '#6181B6'
+        },
+        {
+            name: 'Python',
+            referenceName: 'python',
+            color: '#386F9F'
+        },
+        {
+            name: 'Java',
+            referenceName: 'java',
+            color: '#EA2D2E'
+        },
+        {
+            name: 'C',
+            referenceName: 'c',
+            color: '#03599C'
+        },
+        {
+            name: 'C++',
+            referenceName: 'cplusplus',
+            color: '#9C033A'
+        },
+        {
+            name: 'C#',
+            referenceName: 'csharp',
+            color: '#68217A'
+        },
+        {
+            name: '.NET',
+            referenceName: 'dotnet',
+            color: '#198AD0'
+        },
+        {
+            name: 'Arch Linux',
+            referenceName: 'archlinux',
+            color: '#1793D1'
+        },
+        {
+            name: 'Ubuntu',
+            referenceName: 'ubuntu',
+            color: '#DD4814'
+        },
+        {
+            name: 'Windows',
+            referenceName: 'windows',
+            color: '#0078D4'
+        },
+        {
+            name: 'SQL Server',
+            referenceName: 'sqlserver',
+            color: '#A91D22'
+        },
+        {
+            name: 'MySQl',
+            referenceName: 'mysql',
+            color: '#00618A'
+        },
+        {
+            name: 'PostgreSQL',
+            referenceName: 'postgresql',
+            color: '#336791'
+        },
+        {
+            name: 'Docker',
+            referenceName: 'docker',
+            color: '#028BB8'
+        },
+        {
+            name: 'Git',
+            referenceName: 'git',
+            color: '#F34F29'
+        },
         ]
     },
     english: {
         informationText: 'During my studies, I learned several programming languages, new technologies and operating systems. The knowledge acquired, technologies and languages mastered are presented here.',
         languages: [{
-                name: 'HTML',
-                referenceName: 'html',
-                percentage: ((60 / 117) * 100).toFixed(2),
-                color: '#E44D26'
-            },
-            {
-                name: 'CSS',
-                referenceName: 'css',
-                percentage: ((137 / 228) * 100).toFixed(2),
-                color: '#1572B6'
-            },
-            {
-                name: 'JavaScript',
-                referenceName: 'javascript',
-                percentage: ((46 / 65) * 100).toFixed(2),
-                color: '#F0DB4F'
-            },
-            {
-                name: 'TypeScript',
-                referenceName: 'typescript',
-                percentage: ((8 / 16) * 100).toFixed(2),
-                color: '#007ACC'
-            },
-            {
-                name: 'React JS',
-                referenceName: 'reactjs',
-                percentage: ((9 / 12) * 100).toFixed(2),
-                color: '#61DAFB'
-            },
-            {
-                name: 'Next JS',
-                referenceName: 'nextjs',
-                percentage: ((6 / 17) * 100).toFixed(2),
-                color: 'var(--text)'
-            },
-            {
-                name: 'Node JS',
-                referenceName: 'nodejs',
-                percentage: ((3 / 59) * 100).toFixed(2),
-                color: '#83CD29'
-            },
-            {
-                name: 'Sass',
-                referenceName: 'sass',
-                percentage: ((4 / 60) * 100).toFixed(2),
-                color: '#CB6699'
-            },
-            {
-                name: 'Bootstrap',
-                referenceName: 'bootstrap',
-                percentage: ((15 / 21) * 100).toFixed(2),
-                color: '#7952B3'
-            },
-            {
-                name: 'jQuery',
-                referenceName: 'jquery',
-                percentage: ((80 / 318) * 100).toFixed(2),
-                color: '#0868AC'
-            },
-            {
-                name: 'Flask',
-                referenceName: 'flask',
-                percentage: ((6 / 16) * 100).toFixed(2),
-                color: 'var(--text)'
-            },
-            {
-                name: 'PHP',
-                referenceName: 'php',
-                percentage: ((8 / 22) * 100).toFixed(2),
-                color: '#6181B6'
-            },
-            {
-                name: 'Python',
-                referenceName: 'python',
-                percentage: ((30 / 317) * 100).toFixed(2),
-                color: '#386F9F'
-            },
-            {
-                name: 'Java',
-                referenceName: 'java',
-                percentage: ((12 / 31) * 100).toFixed(2),
-                color: '#EA2D2E'
-            },
-            {
-                name: 'C',
-                referenceName: 'c',
-                percentage: ((10 / 15) * 100).toFixed(2),
-                color: '#03599C'
-            },
-            {
-                name: 'C++',
-                referenceName: 'cplusplus',
-                percentage: ((11 / 25) * 100).toFixed(2),
-                color: '#9C033A'
-            },
-            {
-                name: 'C#',
-                referenceName: 'csharp',
-                percentage: ((7 / 14) * 100).toFixed(2),
-                color: '#68217A'
-            },
-            {
-                name: '.NET',
-                referenceName: 'dotnet',
-                percentage: ((13 / 182) * 100).toFixed(2),
-                color: '#198AD0'
-            },
-            {
-                name: 'Arch Linux',
-                referenceName: 'archlinux',
-                percentage: ((15 / 22) * 100).toFixed(2),
-                color: '#1793D1'
-            },
-            {
-                name: 'Ubuntu',
-                referenceName: 'ubuntu',
-                percentage: ((6 / 8) * 100).toFixed(2),
-                color: '#DD4814'
-            },
-            {
-                name: 'Windows',
-                referenceName: 'windows',
-                percentage: ((4 / 8) * 100).toFixed(2),
-                color: '#0078D4'
-            },
-            {
-                name: 'SQL Server',
-                referenceName: 'sqlserver',
-                percentage: ((28 / 38) * 100).toFixed(2),
-                color: '#A91D22'
-            },
-            {
-                name: 'MySQl',
-                referenceName: 'mysql',
-                percentage: ((28 / 38) * 100).toFixed(2),
-                color: '#00618A'
-            },
-            {
-                name: 'PostgreSQL',
-                referenceName: 'postgresql',
-                percentage: ((28 / 38) * 100).toFixed(2),
-                color: '#336791'
-            },
-            {
-                name: 'Docker',
-                referenceName: 'docker',
-                percentage: ((5 / 8) * 100).toFixed(2),
-                color: '#028BB8'
-            },
-            {
-                name: 'Git',
-                referenceName: 'git',
-                percentage: ((19 / 44) * 100).toFixed(2),
-                color: '#F34F29'
-            },
+            name: 'HTML',
+            referenceName: 'html',
+            color: '#E44D26'
+        },
+        {
+            name: 'CSS',
+            referenceName: 'css',
+            color: '#1572B6'
+        },
+        {
+            name: 'JavaScript',
+            referenceName: 'javascript',
+            color: '#F0DB4F'
+        },
+        {
+            name: 'TypeScript',
+            referenceName: 'typescript',
+            color: '#007ACC'
+        },
+        {
+            name: 'React JS',
+            referenceName: 'reactjs',
+            color: '#61DAFB'
+        },
+        {
+            name: 'Next JS',
+            referenceName: 'nextjs',
+            color: 'var(--text)'
+        },
+        {
+            name: 'Node JS',
+            referenceName: 'nodejs',
+            color: '#83CD29'
+        },
+        {
+            name: 'Sass',
+            referenceName: 'sass',
+            color: '#CB6699'
+        },
+        {
+            name: 'Bootstrap',
+            referenceName: 'bootstrap',
+            color: '#7952B3'
+        },
+        {
+            name: 'jQuery',
+            referenceName: 'jquery',
+            color: '#0868AC'
+        },
+        {
+            name: 'Flask',
+            referenceName: 'flask',
+            color: 'var(--text)'
+        },
+        {
+            name: 'PHP',
+            referenceName: 'php',
+            color: '#6181B6'
+        },
+        {
+            name: 'Python',
+            referenceName: 'python',
+            color: '#386F9F'
+        },
+        {
+            name: 'Java',
+            referenceName: 'java',
+            color: '#EA2D2E'
+        },
+        {
+            name: 'C',
+            referenceName: 'c',
+            color: '#03599C'
+        },
+        {
+            name: 'C++',
+            referenceName: 'cplusplus',
+            color: '#9C033A'
+        },
+        {
+            name: 'C#',
+            referenceName: 'csharp',
+            color: '#68217A'
+        },
+        {
+            name: '.NET',
+            referenceName: 'dotnet',
+            color: '#198AD0'
+        },
+        {
+            name: 'Arch Linux',
+            referenceName: 'archlinux',
+            color: '#1793D1'
+        },
+        {
+            name: 'Ubuntu',
+            referenceName: 'ubuntu',
+            color: '#DD4814'
+        },
+        {
+            name: 'Windows',
+            referenceName: 'windows',
+            color: '#0078D4'
+        },
+        {
+            name: 'SQL Server',
+            referenceName: 'sqlserver',
+            color: '#A91D22'
+        },
+        {
+            name: 'MySQl',
+            referenceName: 'mysql',
+            color: '#00618A'
+        },
+        {
+            name: 'PostgreSQL',
+            referenceName: 'postgresql',
+            color: '#336791'
+        },
+        {
+            name: 'Docker',
+            referenceName: 'docker',
+            color: '#028BB8'
+        },
+        {
+            name: 'Git',
+            referenceName: 'git',
+            color: '#F34F29'
+        },
         ]
     }
 }
